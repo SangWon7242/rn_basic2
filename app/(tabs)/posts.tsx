b50e6fed86e3a4c12bc8982c3fc705e6406a0687
SangWon7242 <gotkdqja@gmail.com>
2025-07-23 16:58:11 +0900

    fetch로 데이터를 가져올 수 있기에 useEffet를 이요하여 데이터 설정

--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,14 +1,18 @@
+import { useEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function Posts() {
-  // 하드코딩 : 데이터를 직접 작성
-  const posts = [
-    { id: 1, title: "게시글 1" },
-    { id: 2, title: "게시글 2" },
-    { id: 3, title: "게시글 3" },
-    { id: 4, title: "게시글 4" },
-    { id: 5, title: "게시글 5" },
-  ];
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    setPosts([
+      { id: 1, title: "게시글 1" },
+      { id: 2, title: "게시글 2" },
+      { id: 3, title: "게시글 3" },
+      { id: 4, title: "게시글 4" },
+      { id: 5, title: "게시글 5" },
+    ]);
+  }, []);
 
   return (
     <View style={styles.postsContainer}>
