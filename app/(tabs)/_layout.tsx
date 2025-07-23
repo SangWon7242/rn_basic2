b752ffc2b803aea12a37e0b1d837d6f630c883e1
SangWon7242 <gotkdqja@gmail.com>
2025-07-23 16:45:13 +0900

    하드코딩한 게시물 정보를 map을 통해 표현

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,6 +3,7 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
+    // Tabs screenOptions={{ headerShown: false }} : 헤더를 없앤다.
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen
         name="home"
--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,9 +1,20 @@
 import { Text, View } from "react-native";
 
 export default function Posts() {
+  // 하드코딩 : 데이터를 직접 작성
+  const posts = [
+    { id: 1, title: "게시글 1" },
+    { id: 2, title: "게시글 2" },
+    { id: 3, title: "게시글 3" },
+    { id: 4, title: "게시글 4" },
+    { id: 5, title: "게시글 5" },
+  ];
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>게시글</Text>
+      {posts.map((post) => (
+        <Text key={post.id}>{post.title}</Text>
+      ))}
     </View>
   );
 }
