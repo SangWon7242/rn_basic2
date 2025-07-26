ebd768b5eeb41ca46db7760b1df20956d9deb89a
SangWon7242 <gotkdqja@gmail.com>
2025-07-26 22:52:45 +0900

    form footer 추가 및 close 버튼 클릭시 게시물 목록으로 이동

--- components/post-write-form.tsx
+++ components/post-write-form.tsx
@@ -1,4 +1,6 @@
 import { Ionicons } from "@expo/vector-icons";
+import Octicons from "@expo/vector-icons/Octicons";
+import { useRouter } from "expo-router";
 import {
   KeyboardAvoidingView,
   Platform,
@@ -11,6 +13,8 @@ import {
 } from "react-native";
 
 export default function PostWriteForm() {
+  const router = useRouter();
+
   const handleSubmit = () => {
     console.log("완료");
   };
@@ -19,12 +23,23 @@ export default function PostWriteForm() {
     console.log("카테고리 선택");
   };
 
+  const selectImage = () => {
+    console.log("사진 선택");
+  };
+
+  const selectHashtag = () => {
+    console.log("해시태그 선택");
+  };
+
   return (
     <View style={styles.formContainer}>
       <View style={styles.formInner}>
         <View style={styles.formHeader}>
           <View style={styles.headerTop}>
-            <Ionicons name="close" size={24} color="black" />
+            {/* navigation.goBack() : 뒤로가기 */}
+            <Pressable onPress={() => router.navigate("/(tabs)/posts/page")}>
+              <Ionicons name="close" size={24} color="black" />
+            </Pressable>
             <Pressable onPress={handleSubmit}>
               <Text style={styles.formSubmit}>완료</Text>
             </Pressable>
@@ -40,10 +55,10 @@ export default function PostWriteForm() {
         </View>
         <KeyboardAvoidingView
           style={styles.formBody}
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          behavior={Platform.OS === "ios" ? "padding" : undefined}
           keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
         >
-          <ScrollView>
+          <ScrollView contentContainerStyle={styles.formBodyInner}>
             <View style={styles.formTitleContainer}>
               <TextInput
                 style={styles.formTitleInput}
@@ -63,6 +78,16 @@ export default function PostWriteForm() {
               />
             </View>
           </ScrollView>
+          <View style={styles.contentFooter}>
+            <Pressable style={styles.footerItem} onPress={selectImage}>
+              <Ionicons name="camera-outline" size={20} color="black" />
+              <Text style={styles.footerText}>사진</Text>
+            </Pressable>
+            <Pressable style={styles.footerItem} onPress={selectHashtag}>
+              <Octicons name="hash" size={20} color="black" />
+              <Text style={styles.footerText}>해시태그</Text>
+            </Pressable>
+          </View>
         </KeyboardAvoidingView>
       </View>
     </View>
@@ -104,7 +129,9 @@ const styles = StyleSheet.create({
   formBody: {
     paddingTop: 10,
     marginHorizontal: 10,
-    flexGrow: 1,
+    flex: 1,
+  },
+  formBodyInner: {
     flex: 1,
   },
   formTitleContainer: {},
@@ -113,10 +140,27 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   formContentContainer: {
-    flexGrow: 1,
+    flex: 1,
   },
   formContentInput: {
     fontSize: 13,
     flex: 1,
+  },
+  contentFooter: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+  },
+  footerItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 10,
+  },
+  footerText: {
+    marginLeft: 5,
+    fontSize: 13,
   },
 });
